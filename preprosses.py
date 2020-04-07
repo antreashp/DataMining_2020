4088ff6306dbac663160d77d9bcb6d53a6841869
Peter Santos <AIMEPTSTSAM@mail.com>
2020-04-07 22:46:21 +0200

    Update preprosses.py

--- preprosses.py
+++ preprosses.py
@@ -8,24 +8,12 @@ import ast
 import matplotlib.pyplot as plt
 import pickle
 
-# #--------------------------------------
-# #                PICKLE LOAD
-# with open('RAW_Data.pickle') as f:
-#     loaded_obj = pickle.load(f)
 
-# print 'loaded_obj is', loaded_obj
-# #--------------------------------------
-
-
-# >>> x = u'[ "A","B","C" , " D"]'
 var_ids = ['mood', 'circumplex.arousal', 'circumplex.valence', 'activity', 'screen', 'call', 'sms', 'appCat.builtin',
            'appCat.communication', 'appCat.entertainment', 'appCat.finance', 'appCat.game', 'appCat.office',
            'appCat.other', 'appCat.social', 'appCat.travel', 'appCat.unknown', 'appCat.utilities', 'appCat.weather',
            'morning', 'noon', 'afternoon', 'night', 'winter', 'spring', 'summer', 'fall']
 
-
-# idx = var_ids.index(var)
-# data = [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None]
 
 class preprocess:
     def __init__(self, filename, window_size=1, step_size=1):
@@ -40,9 +28,45 @@ class preprocess:
         self.window = window_size if window_size >= 1 and type(window_size) is int else 1
         self.step = step_size if step_size is step_size <= window_size and step_size >= 1 and type(
             step_size) is int else 1
+        self.processed_data = {}
 
     def average(self, record):
-        return [sum([x[i] for x in record]) / len(record) for i in range(len(record[0]))]
+        """
+        :param record: list
+        """
+        return [sum([x[i] for x in record if x[i] is not None]) / len([x for x in record if x[i] is not None])
+                for i in range(3, 19)]
+
+    def average_mood(self, record):
+        """
+        :param record: list
+        """
+        moods = []
+        for data_point in record:
+            if data_point[0] is not None:
+                moods.append(data_point[0])
+        return sum(moods) / len(moods) if len(moods) else -1
+
+    def average_circumplex(self, record):
+        """
+        :param record: list
+        """
+        arousal = []
+        valence = []
+        for data_point in record:
+            if data_point[1] is not None:
+                arousal.append(data_point[1])
+        for data_point in record:
+            if data_point[2] is not None:
+                valence.append(data_point[2])
+        return sum(arousal) / len(arousal) if len(arousal) else 0.5, sum(valence) / len(valence) if len(valence) else\
+            0.5
+
+    def average_time_and_season(self, record):
+        """
+        :param record: list
+        """
+        return [sum([x[i] for x in record]) / len(record) for i in range(19, 27)]
 
     def normalize(self):
         for user in self.data.keys():
@@ -52,7 +76,7 @@ class preprocess:
             for date in date_keys:
                 date_data = user_data[date]
                 for record in date_data:
-                    for i in range(3, 18):
+                    for i in range(3, 19):
                         if record[i] is None:
                             record[i] = 0
                         if max_values[i] < record[i]:
@@ -61,10 +85,9 @@ class preprocess:
                 date_data = user_data[date]
                 for i in range(len(date_data)):
                     record = date_data[i]
-                    for j in range(3, 18):
+                    for j in range(3, 19):
                         record[j] = record[j] / max_values[j] if max_values[j] != 0 else 0
                     self.data[user][date][i] = record
-            print(self.data[user])
 
     def bin(self, include_remainder=False):
         for user in self.data.keys():
@@ -72,41 +95,44 @@ class preprocess:
             current_index = 0
             processed_user_data = {}
             date_keys = [x for x in user_data.keys()]
-            while (current_index + + self.window < len(user_data) and not include_remainder) \
+            while (current_index + self.window < len(user_data.items()) and not include_remainder) \
                     or (current_index + self.step < len(user_data) and include_remainder):
                 record = []
                 if current_index + self.window >= len(user_data) and include_remainder:
                     for i in range(len(user_data) - current_index):
-                        record.append(user_data[date_keys[current_index + i]])
+                        for j in range(len(user_data[date_keys[current_index + i]])):
+                            record.append(user_data[date_keys[current_index + i]][j])
                 else:
                     for i in range(self.window):
-                        record.append(user_data[date_keys[current_index + i]])
+                        for j in range(len(user_data[date_keys[current_index + i]])):
+                            record.append(user_data[date_keys[current_index + i]][j])
+                data_point = [self.average_mood(record)] + list(self.average_circumplex(record)) + self.average(
+                    record) + self.average_time_and_season(record)
+                if data_point[0] == -1:
+                    current_index += self.step
+                    continue
+                processed_user_data[date_keys[current_index]] = data_point
                 current_index += self.step
+            self.processed_data[user] = processed_user_data
+
 
 filename = 'RAW_Data.pickle'
-print(type(1))
 
 if __name__ == '__main__':
     preprocess_instance = preprocess(filename)
     preprocess_instance.normalize()
-    # preprocess_instance.bin()
+    preprocess_instance.bin()
 
-# summ = np.zeros(27)
-# for line in range(len(data)):
-#     date = data[line].split('_')[0]
-#     rest_of_line = data[line].split('_')[1]
-#     # print(data)
-#     id = rest_of_line.split('[[')[0]
-#     x = ast.literal_eval('[['+rest_of_line.split('[[')[1])
-#     mood = x[0]
-#     x= x[1:]
-#     print(date,id,mood, x)
-#     x = np.array(x)
-#     x[x==None] = 0
-#     print(np.max(x,axis=0))
-#     exit()
-#     x = np.array(x)
-#     x[x==None] = 0
-#
-#     x[x!=0] = 1
-#     summ =summ+ np.sum(x,axis=1)
+    '''Save preprocess_instance.processed_data:'''
+    print(preprocess_instance.processed_data)
+
+
+    '''
+    {
+        user_id: {
+            date: {
+                [variables]
+            }, ...
+        }, ...
+    }
+    '''
