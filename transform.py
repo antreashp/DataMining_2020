eaa4dda305b02f05c4e614b696c2ea7b23f474ef
Peter Santos <AIMEPTSTSAM@mail.com>
2020-04-18 13:12:47 +0200

    Update transform.py

--- transform.py
+++ transform.py
@@ -2,6 +2,16 @@ from collections.abc import Iterable
 
 
 class Transform:
+    """
+    Usage:
+        t = Transform(y_train)
+        y_train = t.fit(y_train)
+        y_test = t.fit(y_test)
+        (train model)
+        ...
+
+        y_predict = t.decode(y_predict)
+    """
     def __init__(self, y_train):
         counts = [0 for i in range(9)]
         for y in y_train:
@@ -24,13 +34,38 @@ class Transform:
             transformed_y_train.append(transformed_y)
 
     def fit(self, y_test):
-        transformed_y_test = []
         if isinstance(y_test, Iterable):
+            transformed_y_test = []
             for y in y_test:
                 if y is None:
                     transformed_y_test.append(None)
                 transformed_y = self.offsets[round(y) if y < 9 else 8] + self.K[round(y) if y < 9 else 8] * (y -
                                                                                                             round(y))
                 transformed_y_test.append(transformed_y)
-        return transformed_y_test
-    
+            return transformed_y_test
+        else:
+            if y_test is None:
+                return None
+            return self.offsets[round(y_test) if y_test < 9 else 8] + self.K[round(y_test) if y_test < 9 else 8] * (y_test -
+                                                                                                            round(y_test))
+
+    def __decode_value__(self, value):
+        if value == 9:
+            return 9
+        if value == 0:
+            return 0
+        if value is None:
+            return None
+        k = 0
+        for offset in self.offsets:
+            if value < offset:
+                break
+            k += 1
+        k -= 1
+        original_value = k + (value - self.offsets[k]) / self.K[k]
+        return original_value
+
+    def decode(self, Y):
+        if isinstance(Y, Iterable):
+            return [self.__decode_value__(y) for y in Y]
+        return self.__decode_value__(Y)
